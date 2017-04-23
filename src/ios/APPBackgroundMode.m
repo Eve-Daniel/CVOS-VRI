1945ae466e837bdd790c9ce89b5406e2288fbc14
Eve <sycoraxa@gmail.com>
2017-04-23 19:07:42 +0300

    initial

--- src/ios/APPBackgroundMode.m
+++ src/ios/APPBackgroundMode.m
@@ -69,6 +69,9 @@ NSString* const kAPPBackgroundEventResume = @"resume";
                      selector:@selector(handleAudioSessionInterruption:)
                          name:AVAudioSessionInterruptionNotification
                        object:nil];
+        
+     NSString* js = @"alert('pluinit_cdd');";
+    [self.commandDelegate evalJs:js];
 }
 
 #pragma mark -
